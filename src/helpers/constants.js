ad5049bc0ef804e7441a494982b4f62ae639a9cf
Aditya Dhawan <aditya.dhawan@popxo.com>
2020-01-13 22:44:45 +0530

    Change production env

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,7 +4,7 @@ const envConstants = {
 }
 
 if(process.env.NODE_ENV === 'production') {
-    envConstants.production = 'http://localhost:3001';
+    envConstants.production = 'http://68.183.93.125:3001/';
 }
 
 export default envConstants;
